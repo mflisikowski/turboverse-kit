02762a14edbb7738894080316f2c60f61e6ffdb5
Mateusz Flisikowski <contact@mflisikowski.dev>
2025-02-22 21:56:05 +0100

    style(i18n): Remove underscore prefix from translation files
    - Rename _en.ts to en.ts and _pl.ts to pl.ts
    - Update import paths in index.ts

--- packages/i18n/src/translations/cms/index.ts
+++ packages/i18n/src/translations/cms/index.ts
@@ -2,8 +2,8 @@ import type { TFunction } from '@payloadcms/translations';
 import type { NestedKeysStripped } from '@payloadcms/translations';
 import type { DefaultTranslationKeys } from '@payloadcms/translations';
 
-import { en } from './_en';
-import { pl } from './_pl';
+import { en } from './en';
+import { pl } from './pl';
 
 // TODO: wait for answer on this: https://github.com/payloadcms/payload/issues/9858
 // @ts-expect-error: TFunction type is not automatically merged with the default translations
